937256feb34daf74cb4ea53f02a304d5f8be5995
Subramani E <mdl21ec119@mec.ac.in>
2024-07-30 13:40:04 +0530

    recursion - subsequence

---
+++ recursion/subsequence.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void subsequence(vector<int>& myarray, int index, vector<int>& basearray){
+    if(index>=basearray.size()){
+        for(auto x: myarray){
+            cout<<x<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+    myarray.push_back(basearray[index]);
+    subsequence(myarray, index + 1, basearray);  // take
+    myarray.pop_back();
+    subsequence(myarray, index + 1, basearray); // not take
+}
+
+void subsequenceSum(vector<int>& myarray, int index, vector<int>& basearray){
+    if(index == myarray.size()){
+        // if(current == target ){
+            // cout<<"Target sum "<<target<<": "<<endl;
+            for(auto x: myarray){
+                cout<<x<<" ";
+            }
+            cout<<endl;
+            return;
+        // }
+    }
+    myarray.push_back(basearray[index]);
+    // int mysum = current + basearray[index];
+    subsequenceSum(myarray, index + 1, basearray);  // take
+    myarray.pop_back();
+    // mysum = current - basearray[index];
+    subsequenceSum(myarray, index + 1, basearray); // not take
+}
+
+int main(){
+    vector<int> myarray = {3, 1, 2, 5, 6, 4, 5, 9};
+    vector<int> temp;
+    // int target = 10;
+    // int temp = 0;
+    // subsequence(temp, 0, myarray);
+    subsequenceSum(temp, 0, myarray);
+
+    return 0;
+}
